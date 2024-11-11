8c46c90e68a6b6774b35b9d1637bf1a6b0f3e910
Roman Bliznets <63358740+rbliznets@users.noreply.github.com>
2024-11-11 10:25:04 +0300

    i2c master

---
+++ I2CMaster.cpp
@@ -0,0 +1,143 @@
+/*!
+    \file
+    \brief Класс инициализации i2c в режиме мастер.
+    \authors Близнец Р.А. (r.bliznets@gmail.com)
+    \version 0.1.0.0
+    \date 11.11.2024
+*/
+
+#include "I2CMaster.h"
+#include "esp_log.h"
+
+static const char *TAG = "I2CMaster";
+
+I2CMaster *I2CMaster::theSingleInstance = nullptr;
+
+I2CMaster::I2CMaster() : CLock()
+{
+}
+
+I2CMaster::~I2CMaster()
+{
+}
+
+I2CMaster *I2CMaster::Instance()
+{
+    if (theSingleInstance == nullptr)
+    {
+        theSingleInstance = new I2CMaster();
+    }
+    return theSingleInstance;
+}
+
+void I2CMaster::free()
+{
+    if (theSingleInstance != nullptr)
+    {
+        for (int16_t i = 0; i < I2C_NUM_MAX; i++)
+        {
+            theSingleInstance->release(i);
+        }
+        delete theSingleInstance;
+        theSingleInstance = nullptr;
+    }
+}
+
+bool I2CMaster::init(int16_t i2c_num, int16_t i2c_sda, int16_t i2c_scl)
+{
+    assert(i2c_num >= 0 && i2c_num < I2C_NUM_MAX);
+    assert(i2c_sda >= 0 && i2c_sda < GPIO_NUM_MAX);
+    assert(i2c_scl >= 0 && i2c_scl < GPIO_NUM_MAX);
+
+    if ((m_i2c[i2c_num].i2c_sda >= 0) || (m_i2c[i2c_num].i2c_scl >= 0))
+        return false;
+    else
+    {
+        m_i2c[i2c_num].i2c_sda = i2c_sda;
+        m_i2c[i2c_num].i2c_scl = i2c_scl;
+        m_i2c[i2c_num].count = 0;
+        return true;
+    }
+}
+
+bool I2CMaster::take(int16_t i2c_num)
+{
+    assert(i2c_num >= 0 && i2c_num < I2C_NUM_MAX);
+
+    if ((m_i2c[i2c_num].i2c_sda < 0) || (m_i2c[i2c_num].i2c_scl < 0))
+        return false;
+    else
+    {
+        lock();
+        if (m_i2c[i2c_num].count == 0)
+        {
+            i2c_master_bus_config_t i2c_mst_config = {
+                .i2c_port = i2c_num,
+                .sda_io_num = (gpio_num_t)m_i2c[i2c_num].i2c_sda,
+                .scl_io_num = (gpio_num_t)m_i2c[i2c_num].i2c_scl,
+                .clk_source = I2C_CLK_SRC_DEFAULT,
+                .glitch_ignore_cnt = 7,
+                .intr_priority = 0,
+                .trans_queue_depth = 0,
+                .flags = {1}};
+            esp_err_t err = i2c_new_master_bus(&i2c_mst_config, &m_i2c[i2c_num].bus_handle);
+            if (err != ESP_OK)
+            {
+                unlock();
+                ESP_LOGE(TAG, "Init I2C%d failed", i2c_num);
+                return false;
+            }
+        }
+        m_i2c[i2c_num].count++;
+        unlock();
+        return true;
+    }
+}
+
+bool I2CMaster::add(int16_t i2c_num, const i2c_device_config_t *dev_config, i2c_master_dev_handle_t *ret_handle)
+{
+    assert(i2c_num >= 0 && i2c_num < I2C_NUM_MAX);
+    assert(dev_config != nullptr);
+
+    if ((m_i2c[i2c_num].i2c_sda < 0) || (m_i2c[i2c_num].i2c_scl < 0))
+        return false;
+
+    lock();
+    if (m_i2c[i2c_num].count != 0)
+    {
+        esp_err_t err = i2c_master_bus_add_device(m_i2c[i2c_num].bus_handle, dev_config, ret_handle);
+        if (err != ESP_OK)
+        {
+            unlock();
+            ESP_LOGE(TAG, "I2CMaster::add %d failed", i2c_num);
+            return false;
+        }
+        unlock();
+        return true;
+    }
+    else
+    {
+        unlock();
+        return false;
+    }
+}
+
+void I2CMaster::release(int16_t i2c_num)
+{
+    assert(i2c_num >= 0 && i2c_num < I2C_NUM_MAX);
+
+    lock();
+    if (m_i2c[i2c_num].count != 0)
+    {
+        m_i2c[i2c_num].count--;
+        if (m_i2c[i2c_num].count == 0)
+        {
+            esp_err_t err = i2c_del_master_bus(m_i2c[i2c_num].bus_handle);
+            if (err != ESP_OK)
+            {
+                ESP_LOGE(TAG, "Delete I2C%d failed", i2c_num);
+            }
+        }
+    }
+    unlock();
+}
---
+++ include/I2CMaster.h
@@ -0,0 +1,64 @@
+/*!
+	\file
+	\brief Класс инициализации i2c в режиме мастер.
+    \authors Близнец Р.А. (r.bliznets@gmail.com)
+	\version 0.1.0.0
+	\date 11.11.2024
+*/
+
+#include "driver/i2c_master.h"
+#include "CLock.h"
+
+/// @brief состояние I2C
+struct SI2CMater
+{
+    int16_t i2c_sda = -1; ///< GPIO для I2C SDA
+    int16_t i2c_scl = -1; ///< GPIO для I2C SCL
+    int16_t count = 0; ///< Количество инициализаций
+    i2c_master_bus_handle_t bus_handle; ///< I2C bus handle
+};
+
+/// @brief Класс инициализации i2c в режиме мастер.
+class I2CMaster  : public CLock
+{
+private:
+	static I2CMaster *theSingleInstance; ///< Единственный экземпляр
+
+protected:
+    SI2CMater m_i2c[I2C_NUM_MAX]; ///< Структура для хранения параметров
+
+    /// @brief Конструктор
+    I2CMaster();
+    /// @brief Деструктор
+    ~I2CMaster();
+
+public:
+	/// @brief  Единственный экземпляр класса.
+	/// @return Указатель на I2CMaster
+	static I2CMaster *Instance();
+	/// @brief Освобождение ресурсов задачи
+	static void free();
+
+    /// @brief Инициализация I2C
+    /// @param i2c_num Номер I2C
+    /// @param i2c_sda GPIO для I2C SDA
+    /// @param i2c_scl GPIO для I2C SCL
+    /// @return true - успешно, false - ошибка
+    bool init(int16_t i2c_num, int16_t i2c_sda, int16_t i2c_scl);
+
+    /// @brief зарегистрировать использование I2C
+    /// @param i2c_num Номер I2C
+    /// @return true - успешно, false - ошибка
+    bool take(int16_t i2c_num);
+
+    /// @brief добавить устройство
+    /// @param i2c_num Номер I2C
+    /// @param dev_config конфигурация устройства
+    /// @param ret_handle указатель на возвращаемый handle
+    /// @return true - успешно, false - ошибка
+    bool add(int16_t i2c_num, const i2c_device_config_t *dev_config, i2c_master_dev_handle_t *ret_handle);
+
+    /// @brief отменить регистрацию устройства
+    /// @param i2c_num Номер I2C
+    void release(int16_t i2c_num);
+};
